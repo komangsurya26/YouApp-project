8f2bd9409c757eede6454ca1ae6f3ae24a94584d
komangsuryasedana <komangsuryasedana@mail.com>
2024-04-09 11:57:30 +0800

    feat: logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthModule } from './user/auth/auth.module';
 import { PrismaService } from './prisma.service';
 import { ProfileModule } from './user/profile/profile.module';
 import { ChatModule } from './chat/chat.module';
+import { LoggerMiddleware } from './utils/logger.middleware';
 
 @Module({
   imports: [AuthModule, ProfileModule, ChatModule, ChatModule],
   controllers: [],
   providers: [PrismaService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
